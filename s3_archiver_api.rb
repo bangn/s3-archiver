e8dda618df7f47ea03e1373fa17e9493f7bca557
bangn <thebangnguyen@gmail.com>
2019-11-21 00:19:18 +1100

    Correctly symbolize hash key

--- s3_archiver_api.rb
+++ s3_archiver_api.rb
@@ -1,27 +1,29 @@
-require "roda"
-require "multi_json"
+# frozen_string_literal: true
 
-require "./lib/s3_handler"
+require 'roda'
+require 'multi_json'
+
+require './lib/s3_handler'
 
 class S3ArchiverApi < Roda
   plugin :indifferent_params
   plugin :json
 
   route do |r|
-    r.post "upload" do
-      request_json = MultiJson.load(request.body.read, symbolized_keys: true)
+    r.post 'upload' do
+      request_json = MultiJson.load(request.body.read, symbolize_keys: true)
 
       # TODO: check if file exists
-      filename = request_json["filename"]
+      filename = request_json[:filename]
 
-      s3_handler.upload(ENV.fetch("AWS_BUCKET_NAME"), filename)
+      s3_handler.upload(ENV.fetch('AWS_BUCKET_NAME'), filename)
 
       request_json
     end
 
-    r.post "delete" do
+    r.post 'delete' do
       request_json = MultiJson.load(request.body.read, symbolized_keys: true)
-      filename = request_json["filename"]
+      filename = request_json['filename']
 
       File.rename(filename, "#{filename}.deleted")
 
@@ -31,9 +33,9 @@ class S3ArchiverApi < Roda
 
   def s3_handler
     @handler ||= S3Handler.new(
-      region: ENV.fetch("AWS_REGION"),
-      access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
-      secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+      region: ENV.fetch('AWS_REGION'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
     )
   end
 end
